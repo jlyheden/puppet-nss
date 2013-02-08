1a5a32478ff8be0c6e112964fb46dc5f145d88c2
stanlee <johan@lyheden.com>
2013-02-08 18:41:55 +0100

    doc tidy

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -1,3 +1,25 @@
+# == Class: nss::ldap
+#
+# Installs and enables NSS with LDAP support
+#
+# === Parameters
+#
+# [*ensure*]
+#   Controls the software installation
+#   Valid values: <tt>present</tt>, <tt>absent</tt>, <tt>purge</tt>
+#
+# [*autoupgrade*]
+#   If Puppet should upgrade the software automatically
+#   Valid values: <tt>true</tt>, <tt>false</tt>
+#
+# === Sample usage
+#
+# include nss::ldap
+#
+# === Author
+#
+# Johan Lyheden <johan.lyheden@artificial-solutions.com>
+#
 class nss::ldap ( $ensure = $nss::params::ensure,
                   $autoupgrade = $nss::params::autoupgrade ) inherits nss::params {
   
