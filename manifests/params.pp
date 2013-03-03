32c1f6c48e4601b1b601ddd33339cdc0d930f5ff
jlyheden <johan@lyheden.com>
2013-03-03 14:30:28 +0100

    linting and rdoc

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,35 @@
+# == Class: nss::params
+#
 class nss::params {
-  $valid_databases = [ 'aliases', 'ethers', 'group', 'hosts', 'netgroup', 'networks', 'passwd', 'protocols', 'publickey', 'rpc', 'services', 'shadow' ]
+
+  $valid_databases = [
+    'aliases',
+    'ethers',
+    'group',
+    'hosts',
+    'netgroup',
+    'networks',
+    'passwd',
+    'protocols',
+    'publickey',
+    'rpc',
+    'services',
+    'shadow'
+  ]
+
   $config_file = '/etc/nsswitch.conf'
-  $template = undef
-  $source = undef
+  $template = ''
+  $source = ''
   $ensure = 'present'
   $autoupgrade = false
-  case $::lsbdistcodename {
-    lucid: {
+
+  case $::operatingsystem {
+    'Ubuntu','Debian': {
       $nss_ldap_package = 'libnss-ldapd'
     }
+    default: {
+      fail ("Unsupported operatingsystem ${::operatingsystem}")
+    }
   }
+
 }
